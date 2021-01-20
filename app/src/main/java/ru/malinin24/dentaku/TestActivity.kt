a05c16abe87b53fc610f13ed6e4bd5ca5f9bd787
Ilya Malinin <malinin174@gmail.com>
2021-01-20 23:40:49 +0500

    Add activity_test

---
+++ app/src/main/java/ru/malinin24/dentaku/TestActivity.kt
@@ -0,0 +1,11 @@
+package ru.malinin24.dentaku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
